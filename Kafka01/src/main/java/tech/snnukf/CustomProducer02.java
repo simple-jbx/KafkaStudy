de894ca3c69457363361c48cca32fac5626b811f
simple <jb.xue@qq.com>
2022-02-15 15:31:57 +0800

    first

---
+++ Kafka01/src/main/java/tech/snnukf/CustomProducer02.java
@@ -0,0 +1,49 @@
+package tech.snnukf;
+
+import org.apache.kafka.clients.producer.*;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+
+
+/**
+ * @author simple.jbx
+ * @ClassName CustomProducer
+ * @description 不带回调函数的API
+ * @email jb.xue@qq.com
+ * @github https://github.com/simple-jbx
+ * @date 2022/02/15/ 15:20
+ */
+public class CustomProducer02 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException{
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.232.128:9092");//kafka 集群，broker-list
+        props.put("acks", "all");
+        props.put("retries", 1);//重试次数
+        props.put("batch.size", 16384);//批次大小
+        props.put("linger.ms", 1);//等待时间
+        props.put("buffer.memory", 33554432);//RecordAccumulator 缓冲区大小
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = new
+                KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<String, String>("first",
+                    Integer.toString(i), Integer.toString(i)), new Callback() {
+                //回调函数，该方法会在 Producer 收到 ack 时调用，为异步调用
+                @Override
+                public void onCompletion(RecordMetadata metadata,
+                                         Exception exception) {
+                    if (exception == null) {
+                        System.out.println("success->" + metadata.offset());
+                    } else {
+                        exception.printStackTrace();
+                    }
+                }
+            });
+        }
+        producer.close();
+    }
+}
